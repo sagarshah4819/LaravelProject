fbb1a1890e72e853e82697bbc3bcd7570d1e7313
sagarshah4819 <ss3958@njit.edu>
2018-10-14 11:50:18 -0700

    Task: Created includes,layouts and pages directory. Created head,header and footer files in includes folder.

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">CarRentals</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item ">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a class="nav-link" href="/why-learana">Why CarRentals?</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/connect">Connect</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/get-started">Get Started</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</header>
