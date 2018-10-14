50bcc4f87bcf480fddf0413f3ec6f88c782ed81f
sagarshah4819 <ss3958@njit.edu>
2018-10-14 13:55:20 -0700

    Fix: Fixed home and about page.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.default')
 @section('content')
-    <h1>About CarRentals</h1>
+
+    <br><br><br><br>
+
+    <h1>About CarRentals </h1>
+
+    <br><br><br><br>
+
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Hello World Welcome to the CarRentals</h1>
+
+    <br><br><br><br>
+
+    <h1> Hello World Welcome to the CarRentals</h1>
+
+    <br><br><br><br>
 @stop
