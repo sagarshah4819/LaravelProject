751448d81610a5db3dcd7c32974596efb8153955
sagarshah4819 <ss3958@njit.edu>
2018-10-14 12:58:49 -0700

    Fix: Added the route to home page and about page in web.php.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="row mt-4 mb-4"  class="font-weight-light">
-    &copy; 2018-2019 IS601 Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
+    &copy; CarRentals Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
 </div>
