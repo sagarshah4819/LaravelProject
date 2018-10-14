70bac2878d593ab5966ad1a50e658e703a516ab3
sagarshah4819 <ss3958@njit.edu>
2018-10-14 12:49:23 -0700

    Task: Created home page and about page in pages folder.

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>About CarRentals</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>Hello World Welcome to the CarRentals</h1>
+@stop
