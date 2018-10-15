01faa14052a980a95da9a296c513c5d538a632f8
sagarshah4819 <ss3958@njit.edu>
2018-10-14 20:02:44 -0700

    Fix: Added the content on the pages.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,6 +4,14 @@
     <br><br><br><br>
 
     <h1>About CarRentals </h1>
+    <br>
+    <h3>Our Website ("Website") is provided solely to assist customers in gathering travel information,
+        determining the availability of travel-related goods and services, making legitimate reservations
+        or otherwise transacting business with travel suppliers, and for no other purposes. The terms "we,"
+        "us," "our," "CarRentals," and "CarRentals.com," refer to CarRentals.com, Inc., a Nevada corporation,
+        and its subsidiaries and its corporate affiliates (collectively the "CarRentals.com Companies"). The
+        term "you" refers to the customer visiting the Website and/or booking a reservation through us on this
+        Website, or through our customer service agents.</h3>
 
     <br><br><br><br>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,13 @@
 
     <br><br><br><br>
 
-    <h1> Hello World Welcome to the CarRentals</h1>
+    <h1> Hello World Welcome to the CarRentals.</h1>
+    <br>
+    <h3>If you don’t own a car in New York,
+        rent one instead to head out of SoHo and into the Hamptons. Planning a family vacation to the beach?
+        Explore Miami’s South Beach or Little Havana on four wheels, or take advantage of a rental car deal
+        in Los Angeles to take your kids to the theme parks. Having a family reunion in San Francisco? Point
+        your wheels west to reach the City by the Bay. Drive the iconic Golden Gate Bridge and visit Alcatraz.</h3>
 
-    <br><br><br><br>
+    <br><br><br><br><br><br><br><br>
 @stop
